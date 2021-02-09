a6b8f28135365e06954c72b3d6316e9304caff7b
joverzhang <joverzh@gmail.com>
2021-02-09 18:00:30 +0800

    First commit

---
+++ src/main/java/com/learn/mybatis/Main.java
@@ -0,0 +1,18 @@
+package com.learn.mybatis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jover Zhang
+ */
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
