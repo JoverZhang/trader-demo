d8e180aa4c463ab64e9f5e830fda522a7e3eb615
joverzhang <joverzh@gmail.com>
2021-02-10 17:25:59 +0800

    test: add `LimitBuyServiceTest` and `LimitSellServiceTest`

--- src/test/java/com/learn/mybatis/service/LimitBuyServiceTest.java
+++ src/test/java/com/learn/mybatis/service/LimitBuyServiceTest.java
@@ -1,113 +1,78 @@
 package com.learn.mybatis.service;
 
 import com.learn.mybatis.MainTest;
-import com.learn.mybatis.core.lua.OrderPoolLuaHelper;
-import com.learn.mybatis.core.lua.OrderPoolMirror;
+import com.learn.mybatis.core.support.LimitBuyOrderPool;
+import com.learn.mybatis.core.support.LimitSellOrderPool;
 import com.learn.mybatis.domain.Order;
 import lombok.AllArgsConstructor;
-import org.junit.jupiter.api.AfterEach;
+import lombok.Getter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 @SpringBootTest(classes = MainTest.class)
 class LimitBuyServiceTest extends Assertions {
 
     @Autowired
-    StringRedisTemplate redisTemplate;
-
-    @Autowired
-    OrderPoolLuaHelper helper;
-
-    @Autowired
     LimitBuyService limitBuyService;
+
+    @MockBean // no override
+    LimitBuyOrderPool limitBuyOrderPool;
+
+    @MockBean
+    LimitSellOrderPool limitSellOrderPool;
 
     @Test
     void entry() {
-        ArrayList<Order> orders = new ArrayList<Order>() {{
-            for (int i = 1; i <= 10; i++) {
-                add(new Order("s" + i, String.valueOf(i), "10"));
-            }
+        List<TestSet> testSets = new LinkedList<TestSet>() {{
+            add(new TestSet(
+                    new Order("b1", "100", "0"),
+                    Collections.emptyList(),
+                    new Order("b1", "100", "0")));
+            add(new TestSet(
+                    new Order("b1", "100", "100"),
+                    new LinkedList<Order>() {{
+                        add(new Order("s1", "100", "20"));
+                        add(new Order("s2", "100", "20"));
+                    }},
+                    new Order("b1", "100", "60")));
+            add(new TestSet(
+                    new Order("b1", "100", "100"),
+                    new LinkedList<Order>() {{
+                        add(new Order("s1", "100", "50"));
+                        add(new Order("s2", "100", "50"));
+                    }},
+                    new Order("b1", "100", "0")));
         }};
-        Map<Order, BindResult> toBeMatchedOrderMap = new LinkedHashMap<Order, BindResult>() {{
-            // 外单价格不足
-            put(new Order("b1", "0.5", "10000"), new BindResult(
-                    new Order("b1", "0.5", "10000"),
-                    Collections.emptyList()));
-            // 完整匹配
-            put(new Order("b2", "2", "20"), new BindResult(
-                    new Order("b2", "2", "0"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s1", "1", "10"));
-                        add(new Order("s2", "2", "10"));
-                    }}));
-            // 外单数量购尽
-            put(new Order("b3", "10000", "15"), new BindResult(
-                    new Order("b3", "10000", "0"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s3", "3", "10"));
-                        add(new Order("s4", "4", "5"));
-                    }}));
-            // 内单数量不足, 外单数量有余
-            put(new Order("b4", "5.5", "10000"), new BindResult(
-                    new Order("b4", "5.5", "9985"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s4", "4", "5"));
-                        add(new Order("s5", "5", "10"));
-                    }}));
-            // 高精度测试 1
-            put(new Order("b5", "7.000001", "19.999999"), new BindResult(
-                    new Order("b5", "7.000001", "0"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s6", "6", "10"));
-                        add(new Order("s7", "7", "9.999999"));
-                    }}));
-            // 高精度测试 2
-            put(new Order("b6", "9.000001", "10.000002"), new BindResult(
-                    new Order("b6", "9.000001", "0"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s7", "7", "0.000001"));
-                        add(new Order("s8", "8", "10"));
-                        add(new Order("s9", "9", "0.000001"));
-                    }}));
-            // 高精度测试 3 购尽内单
-            put(new Order("b5", "10.000001", "10000"), new BindResult(
-                    new Order("b5", "10.000001", "9980.000001"),
-                    new LinkedList<Order>() {{
-                        add(new Order("s9", "9", "9.999999"));
-                        add(new Order("s10", "10", "10"));
-                    }}));
-        }};
-        // Mirror
-        {
-            orders.forEach(limitBuyService.limitSellOrderPool::addOrder);
-            OrderPoolMirror mirrorForLimitBuyOrderPool = new OrderPoolMirror();
 
-            toBeMatchedOrderMap.forEach((order, result) -> {
-                assertEquals(result.matchedOrders, limitBuyService.entry(order));
-                mirrorForLimitBuyOrderPool.addOrder(order);
-                assertEquals(mirrorForLimitBuyOrderPool.getOrderPool(),
-                        helper.fetchOrderPool(limitBuyService.limitBuyOrderPool.getNamespace()));
-            });
+        for (TestSet testSet : testSets) {
+            Mockito.when(limitSellOrderPool.match(testSet.getParam())).thenReturn(testSet.getResult());
+
+            // 该入参会在方法内部被改变
+            Order entryParam = testSet.getParam();
+            List<Order> matchedOrders = limitBuyService.entry(entryParam);
+
+            assertEquals(testSet.getResult(), matchedOrders);
+            assertEquals(testSet.getAfterParam(), entryParam);
         }
     }
 
-    @AfterEach
-    void after() {
-        redisTemplate.execute((RedisCallback<Object>) connection -> connection.execute("FLUSHALL"));
-    }
-
+    @Getter
     @AllArgsConstructor
-    static class BindResult {
+    static class TestSet {
 
-        Order order;
+        Order param;
 
-        List<Order> matchedOrders;
+        List<Order> result;
+
+        Order afterParam;
 
     }
 
--- src/test/java/com/learn/mybatis/service/LimitSellServiceTest.java
+++ src/test/java/com/learn/mybatis/service/LimitSellServiceTest.java
@@ -1,11 +1,21 @@
 package com.learn.mybatis.service;
 
 import com.learn.mybatis.MainTest;
+import com.learn.mybatis.core.support.LimitBuyOrderPool;
+import com.learn.mybatis.core.support.LimitSellOrderPool;
+import com.learn.mybatis.domain.Order;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 @SpringBootTest(classes = MainTest.class)
 class LimitSellServiceTest extends Assertions {
@@ -13,13 +23,57 @@ class LimitSellServiceTest extends Assertions {
     @Autowired
     LimitSellService limitSellService;
 
-    @BeforeEach
-    void before() {
-//        limitSellService.
-    }
+    @MockBean // no override
+    LimitSellOrderPool limitSellOrderPool;
+
+    @MockBean
+    LimitBuyOrderPool limitBuyOrderPool;
 
     @Test
     void entry() {
+        List<TestSet> testSets = new LinkedList<TestSet>() {{
+            add(new TestSet(
+                    new Order("s1", "100", "0"),
+                    Collections.emptyList(),
+                    new Order("s1", "100", "0")));
+            add(new TestSet(
+                    new Order("s1", "100", "100"),
+                    new LinkedList<Order>() {{
+                        add(new Order("b1", "100", "20"));
+                        add(new Order("b2", "100", "20"));
+                    }},
+                    new Order("s1", "100", "60")));
+            add(new TestSet(
+                    new Order("s1", "100", "100"),
+                    new LinkedList<Order>() {{
+                        add(new Order("b1", "100", "50"));
+                        add(new Order("b2", "100", "50"));
+                    }},
+                    new Order("s1", "100", "0")));
+        }};
+
+        for (TestSet testSet : testSets) {
+            Mockito.when(limitBuyOrderPool.match(testSet.getParam())).thenReturn(testSet.getResult());
+
+            // 该入参会在方法内部被改变
+            Order entryParam = testSet.getParam();
+            List<Order> matchedOrders = limitSellService.entry(entryParam);
+
+            assertEquals(testSet.getResult(), matchedOrders);
+            assertEquals(testSet.getAfterParam(), entryParam);
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class TestSet {
+
+        Order param;
+
+        List<Order> result;
+
+        Order afterParam;
+
     }
 
 }
